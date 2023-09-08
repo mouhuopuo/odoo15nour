870a4a2903e34635b40519b183430321b421622c
nourKbaili <142293441+nourKbaili@users.noreply.github.com>
2023-09-08 14:34:32 -0700

    Update hr_leave_allocation.py
    updating methods to handle postponed days

--- ALTANMYA_accrual_plan_extension/models/hr_leave_allocation.py
+++ ALTANMYA_accrual_plan_extension/models/hr_leave_allocation.py
@@ -28,7 +28,6 @@ class HolidaysAllocation(models.Model):
             Method called by the cron task in order to increment the number_of_days when
             necessary.
         """
-        print("HELLO start _update_accrual ")
         # Get the current date to determine the start and end of the accrual period
         today = datetime.combine(fields.Date.today(), time(0, 0, 0))
         this_year_first_day = (today + relativedelta(day=1, month=1)).date()
@@ -44,7 +43,7 @@ class HolidaysAllocation(models.Model):
             current_level = eoy_allocation._get_current_accrual_plan_level_id(this_year_first_day)[0]
             if current_level.action_with_unused_accruals != 'contract_postponed':
                 eoy_allocation._end_of_year_accrual()
-                eoy_allocation.flush_model()
+                eoy_allocation.flush()
 
         # the new implementation
         employees = self.env['hr.employee'].search([])
@@ -75,19 +74,24 @@ class HolidaysAllocation(models.Model):
 
                 if curr_lvl:
                     if curr_lvl.action_with_unused_accruals != 'contract_postponed':
+                        print('hello Im in first if in curr_level altanmya ',curr_lvl.action_with_unused_accruals )
                         if alloc.lastcall < this_year_first_day:
                             alloc._end_of_year_accrual()
-                            alloc.flush_model()
+                            alloc.flush()
                     elif curr_lvl.action_with_unused_accruals == 'contract_postponed':
+                        print('hello Im in elif in curr_level altanmya ', curr_lvl.action_with_unused_accruals)
                         if not current_contract.date_end:
+                            print('hello Im in elif-> not current_contract.date_end  ')
                             alloc._end_of_year_accrual()
-                            alloc.flush_model()
+                            alloc.flush()
                         elif current_contract and current_contract.state == 'open':
+                            print('hello Im in elif--------> current_contract and current_contract.state == open')
                             alloc._end_of_contract_year_accrual(prev_contract.date_end, current_contract.date_start)
-                            alloc.flush_model()
+                            alloc.flush()
                         else:
+                            print('hello Im in elif--------> else')
                             alloc._end_of_year_accrual()
-                            alloc.flush_model()
+                            alloc.flush()
 
 
         allocations = self.search(
@@ -95,9 +99,9 @@ class HolidaysAllocation(models.Model):
              ('employee_id', '!=', False),
              '|', ('date_to', '=', False), ('date_to', '>', fields.Datetime.now()),
              '|', ('nextcall', '=', False), ('nextcall', '<=', today)])
-        allocations._process_accrual_plans()
+        # !!! check point
+        allocations._process_accrual_plans(date_to=False, force_period=False)
         print('all allocation : ', allocations)
-        print("HELLO END _update_accrual ")
 
     def _end_of_contract_year_accrual(self, end_of_active_contract, start_of_new_contract):
         today = fields.Date.today()
@@ -127,7 +131,7 @@ class HolidaysAllocation(models.Model):
                 allocation.write({'number_of_days': number_of_days, 'lastcall': lastcall, 'nextcall': nextcall})
                 allocation.write({'is_grace_period_passed': False, 'last_year_postponed_days': number_of_days})
                 print('allocation : ', allocation)
-
+    # this handles postponed max_days
     def _end_of_year_accrual(self):
         # to override in payroll
         today = fields.Date.today()
@@ -235,3 +239,76 @@ class HolidaysAllocation(models.Model):
                                 allocation.is_grace_period_passed = True
                             else:
                                 allocation.is_grace_period_passed = True
+
+
+    def _process_accrual_plans(self, date_to=False, force_period=False):
+        print("Altanmya _____hello i'm in _process_accrual_plans")
+        """
+        This method is part of the cron's process.
+        The goal of this method is to retroactively apply accrual plan levels and progress from nextcall to date_to or today.
+        If force_period is set, the accrual will run until date_to in a prorated way (used for end of year accrual actions).
+        """
+        date_to = date_to or fields.Date.today()
+        first_allocation = _("""This allocation have already ran once, any modification won't be effective to the days allocated to the employee. If you need to change the configuration of the allocation, cancel and create a new one.""")
+        for allocation in self:
+            level_ids = allocation.accrual_plan_id.level_ids.sorted('sequence')
+            if not level_ids:
+                continue
+            if not allocation.nextcall:
+                first_level = level_ids[0]
+                first_level_start_date = allocation.date_from + get_timedelta(first_level.start_count, first_level.start_type)
+                if date_to < first_level_start_date:
+                    # Accrual plan is not configured properly or has not started
+                    continue
+                allocation.lastcall = max(allocation.lastcall, first_level_start_date)
+                allocation.nextcall = first_level._get_next_date(allocation.lastcall)
+                if len(level_ids) > 1:
+                    second_level_start_date = allocation.date_from + get_timedelta(level_ids[1].start_count, level_ids[1].start_type)
+                    allocation.nextcall = min(second_level_start_date, allocation.nextcall)
+                allocation._message_log(body=first_allocation)
+            days_added_per_level = defaultdict(lambda: 0)
+            while allocation.nextcall <= date_to:
+                (current_level, current_level_idx) = allocation._get_current_accrual_plan_level_id(allocation.nextcall)
+                nextcall = current_level._get_next_date(allocation.nextcall)
+                # Since _get_previous_date returns the given date if it corresponds to a call date
+                # this will always return lastcall except possibly on the first call
+                # this is used to prorate the first number of days given to the employee
+                period_start = current_level._get_previous_date(allocation.lastcall)
+                period_end = current_level._get_next_date(allocation.lastcall)
+                # Also prorate this accrual in the event that we are passing from one level to another
+                if current_level_idx < (len(level_ids) - 1) and allocation.accrual_plan_id.transition_mode == 'immediately':
+                    next_level = level_ids[current_level_idx + 1]
+                    current_level_last_date = allocation.date_from + get_timedelta(next_level.start_count, next_level.start_type)
+                    if allocation.nextcall != current_level_last_date:
+                        nextcall = min(nextcall, current_level_last_date)
+                # We have to check for end of year actions if it is within our period
+                #  since we can create retroactive allocations.
+                if allocation.lastcall.year < allocation.nextcall.year and\
+                    current_level.action_with_unused_accruals == 'postponed' and\
+                    current_level.postpone_max_days > 0:
+                    # Compute number of days kept
+                    allocation_days = allocation.number_of_days - allocation.leaves_taken
+                    allowed_to_keep = max(0, current_level.postpone_max_days - allocation_days)
+                    number_of_days = min(allocation_days, current_level.postpone_max_days)
+                    allocation.number_of_days = number_of_days + allocation.leaves_taken
+                    total_gained_days = sum(days_added_per_level.values())
+                    days_added_per_level.clear()
+                    days_added_per_level[current_level] = min(total_gained_days, allowed_to_keep)
+                gained_days = allocation._process_accrual_plan_level(
+                    current_level, period_start, allocation.lastcall, period_end, allocation.nextcall)
+                days_added_per_level[current_level] += gained_days
+                if current_level.maximum_leave > 0 and sum(days_added_per_level.values()) > current_level.maximum_leave:
+                    days_added_per_level[current_level] -= sum(days_added_per_level.values()) - current_level.maximum_leave
+
+                allocation.lastcall = allocation.nextcall
+                allocation.nextcall = nextcall
+                if force_period and allocation.nextcall > date_to:
+                    allocation.nextcall = date_to
+                    force_period = False
+
+            if days_added_per_level:
+                number_of_days_to_add = allocation.number_of_days + sum(days_added_per_level.values())
+                # Let's assume the limit of the last level is the correct one
+                allocation.number_of_days = min(number_of_days_to_add, current_level.maximum_leave + allocation.leaves_taken) if current_level.maximum_leave > 0 else number_of_days_to_add
+                print('allocation.number_of_days', allocation.number_of_days)
+        print("odoo _____hello i'm in the end of _process_accrual_plans")
